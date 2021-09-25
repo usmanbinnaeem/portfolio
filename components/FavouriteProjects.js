4ffa5c16d847ba85c83c8de768a1e5bf495b3e27
usmaanbinnaeem <usmaanbinnaeem@gmail.com>
2021-09-25 20:52:41 +0500

    remove typo errors

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -52,6 +52,25 @@ export default function FavouriteProjects() {
               </h1>
             </div>
           </a>
+
+          <a
+            href="https://www.djlightingdiscount.com/"
+            className="w-full block col-span-3 sm:col-span-1  object-cover"
+          >
+            <div className="relative overflow-hidden shadow-2xl">
+              <img
+                src="/djlightingdiscount.png"
+                alt="Ecommerce"
+                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+               Ecommerce website
+              </h1>
+              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                03
+              </h1>
+            </div>
+          </a>
           {/* Single card */}
           <a
             href="https://covid-19-analytics.netlify.app/"
@@ -72,24 +91,7 @@ export default function FavouriteProjects() {
             </div>
           </a>
 
-          <a
-            href="https://gatsby-contentfull-blog.netlify.app/"
-            className="w-full block col-span-3 sm:col-span-1  object-cover"
-          >
-            <div className="relative overflow-hidden shadow-2xl">
-              <img
-                src="/blog-app.png"
-                alt="blog"
-                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
-              />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Simple Auth Blog
-              </h1>
-              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                03
-              </h1>
-            </div>
-          </a>
+         
         </div>
       </div>
     </div>
